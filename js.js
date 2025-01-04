ac778ec9704dcab66888485fac493da5a1fbec3d
Olomr <acura7393@gmail.com>
2025-01-04 15:32:32 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+function switchcolor() {
+    let header = document.querySelector('header')
+    header.style.backgroundColor = 'black'
+}
+let btn = document.querySelector('.switch-button')
+btn.addEventListener('click', switchcolor)
+console.log('123456')
